cbdde9979dc0f4ef3b36c1c28028ef12b61ec622
Geoff Hackworth <geoff@hacknicity.com>
2020-01-11 12:29:02 +0000

    Changed project and test target iOS Deployment Target to iOS 11, used availability checks to fix build errors, and added window property to AppDelegate and TestAppDelegate.

--- TestingSceneDelegateObjCTests/TestingAppDelegate.h
+++ TestingSceneDelegateObjCTests/TestingAppDelegate.h
@@ -10,4 +10,6 @@
 
 @interface TestingAppDelegate : UIResponder<UIApplicationDelegate>
 
+@property (strong, nonatomic) UIWindow * window;
+
 @end
--- TestingSceneDelegateObjCTests/TestingAppDelegate.m
+++ TestingSceneDelegateObjCTests/TestingAppDelegate.m
@@ -9,29 +9,36 @@
 #import "TestingAppDelegate.h"
 
 #import "TestingSceneDelegate.h"
+#import "TestingRootViewController.h"
 
 @implementation TestingAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
 
-    // Remove any cached scene configurations to ensure that TestingAppDelegate.application(_:configurationForConnecting:options:)
-    // is called and TestingSceneDelegate will be used when running unit tests. NOTE: THIS IS PRIVATE API AND MAY BREAK IN THE FUTURE!
+    if (@available(ios 13.0, *)) {
+        // Remove any cached scene configurations to ensure that TestingAppDelegate.application(_:configurationForConnecting:options:)
+        // is called and TestingSceneDelegate will be used when running unit tests. NOTE: THIS IS PRIVATE API AND MAY BREAK IN THE FUTURE!
 
-    // We can't simply use @selector(_removeSessionFromSessionSet:) as that generates an "Undeclared selector" warning.
-    // Therefore we need to create the selector at run time using NSSelectorFromString(). However, the code
-    //
-    //      [application performSelector:NSSelectorFromString(@"_removeSessionFromSessionSet:") withObject:sceneSession]
-    //
-    // also generates a warning ("PerformSelector may cause a leak because its selector is unknown")
-    //
-    // Using a technique from https://stackoverflow.com/a/20058585/870671 we can build a function pointer to the underlying
-    // implementation of _removeSessionFromSessionSet: and call it directly
-    SEL selector = NSSelectorFromString(@"_removeSessionFromSessionSet:");
-    void (*removeSessionFromSessionSet)(UIApplication *, SEL, UISceneSession *) = (void *)[application methodForSelector:selector];
+        // We can't simply use @selector(_removeSessionFromSessionSet:) as that generates an "Undeclared selector" warning.
+        // Therefore we need to create the selector at run time using NSSelectorFromString(). However, the code
+        //
+        //      [application performSelector:NSSelectorFromString(@"_removeSessionFromSessionSet:") withObject:sceneSession]
+        //
+        // also generates a warning ("PerformSelector may cause a leak because its selector is unknown")
+        //
+        // Using a technique from https://stackoverflow.com/a/20058585/870671 we can build a function pointer to the underlying
+        // implementation of _removeSessionFromSessionSet: and call it directly
+        SEL selector = NSSelectorFromString(@"_removeSessionFromSessionSet:");
+        void (*removeSessionFromSessionSet)(UIApplication *, SEL, UISceneSession *) = (void *)[application methodForSelector:selector];
 
-    for (UISceneSession *sceneSession in application.openSessions) {
-        removeSessionFromSessionSet(application, selector, sceneSession);
+        for (UISceneSession *sceneSession in application.openSessions) {
+            removeSessionFromSessionSet(application, selector, sceneSession);
+        }
+    } else {
+        self.window = [[UIWindow alloc] init];
+        self.window.rootViewController = [[TestingRootViewController alloc] init];
+        [self.window makeKeyAndVisible];
     }
 
     return YES;
@@ -41,7 +48,7 @@
 #pragma mark - UISceneSession lifecycle
 
 
-- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
+- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options API_AVAILABLE(ios(13.0)) {
     // Called when a new scene session is being created.
     // Use this method to select a configuration to create the new scene with.
     UISceneConfiguration *sceneConfiguration = [[UISceneConfiguration alloc] initWithName:nil sessionRole:connectingSceneSession.role];
